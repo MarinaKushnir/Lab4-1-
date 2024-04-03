d65ca06660acaf8db1fed1f2d40bbd73cf547cb7
MarinaKushnir <80197488+MarinaKushnir@users.noreply.github.com>
2024-04-03 17:07:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Lab4_1_
 {
@@ -6,9 +6,28 @@ namespace Lab4_1_
     {
         public static void Main(string[] args)
         {
-            Lines lines = new Lines(2, 3, 4, 5, 1, 6, 7, 8);
+            Lines AB = new Lines(1, 3, 3, 6);
+            Lines CD = new Lines(2, 5, 4, 8);
 
+            Console.WriteLine("Уровнение прямой AB:");
+            AB.Equation();
+            Console.WriteLine("Уровнение прямой CD:");
+            CD.Equation();
+
+            
+            if (AB == CD )
+            {
+                Console.WriteLine($"Прямые AB и CD параллельны.");
+            }
+            else
+            {
+                Console.WriteLine("Прямые AB и CD не параллельны.");
+                double angle = Lines.Angle(AB, CD);
+                Console.WriteLine($"Угол между прямыми AB и CD: {angle} градусов.");
+            }
         }
+
+        
     }
 
 }
