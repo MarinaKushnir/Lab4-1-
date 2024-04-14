c548eab88c0705828ddc5c38f5e121391e00fc92
MarinaKushnir <80197488+MarinaKushnir@users.noreply.github.com>
2024-04-14 21:24:00 +0300

    Update and rename Lines.cs to Point.cs

---
+++ Point.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4_1_
+{
+    public class Point
+    {
+        public int x, y;
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
